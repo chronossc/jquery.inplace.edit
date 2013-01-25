0f7cea58c0504305e896ad15a3a6fdd2a6a69d4e
Felipe Prenholato <felipe.rafael@pdg.com.br>
2013-01-25 17:40:15 -0200

    editor working.

--- jquery.inplace.edit.js
+++ jquery.inplace.edit.js
@@ -20,12 +20,16 @@
     // Create the defaults once
     var pluginName = "inplaceEdit",
         defaults = {
-            propertyName: "value"
+            css_main_container_class: "inlineEdit",
+            css_value_container_class: "inlineValue",
+            css_input_container_class: "inlineInput",
+            css_input_classes: ""
         };
 
     // The actual plugin constructor
     function Plugin( element, options ) {
-        this.element = element;
+
+        this.$el = $(element);
 
         // jQuery has an extend method which merges the contents of two or
         // more objects, storing the result in the first object. The first object
@@ -48,16 +52,155 @@
             // and this.options
             // you can add more functions like the one below and
             // call them like so: this.yourOtherFunction(this.element, this.options).
-            console.log('init');
-            this._run('yourOtherFunction', this.element, this.options);
+
+
+            // set value so we can use it from there instead el.text().
+            // this.value is always updated with value get from input.
+            this.value = this.$el.text();
+
+            // element get opts.css_main_container_class
+            this.$el.addClass(this.options.css_main_container_class);
+            this.html = $(this._get_func('getHTML').apply(this, [this.$el, this.options]));
+            this.html.filter("." + this.options.css_input_container_class).hide();
+            this.$el.html(this.html)
+                .bind('click.' + pluginName, $.proxy(this._get_func('onClick'), this));
+            return this;
         },
-        _run: function() {
-            // wraper to run custom methods provided by in this.options[methodName]
-            return ((this.options[arguments[0]]) ? this.options[arguments[0]] : this[arguments[0]])
-                .apply(this, Array.prototype.slice.call( arguments, 1 ));
+
+        _get_func: function(fname) {
+            // wraper to run custom methods provided by user in this.options[methodName] instead ours
+            return ((this.options[fname]) ? this.options[fname] : this[fname]);
+                // .apply(this, Array.prototype.slice.call( arguments, 1 ));
         },
-        yourOtherFunction: function() {
-            console.log('Plugin.yourOtherFunction');
+
+        getHTML: function($el, opts) {
+            /*
+             * Returns HTML that will be used in place of initial value.
+             *
+             * Params:
+             *     @el: element, same as this.$el.
+             *     @opts: plugin options, same as this.options.
+             *
+             *     this is plugin instance.
+             * Returns:
+             *     Plain text HTML
+             *
+             * Tip: if you have coded html already in element, return $(el).html() here.
+             *
+             * You should follow a standard here. We will have two containers,
+             * one will keep value, other will keep input.
+             *
+             * To get value we will do $("." + opts.css_main_container_class + " ." + opts.css_value_container_class).
+             * To get input we will do $("." + opts.css_main_container_class + " ." + opts.css_input_container_class).
+             *
+             * Use opts.css_value_container_class and opts.css_input_container_class to define containers for value and input.
+             * Use this.value to get current value instead $el.text()
+             * Use opts.css_input_classes to get input to be used on classes, so you can set some css of your framework
+             */
+            return "<span class='" + opts.css_value_container_class + "'>" + this.value + "</span>" +
+                   "<span class='" + opts.css_input_container_class + "'><input type='text' class='" +  opts.css_input_classes + "' /></span>";
+        },
+
+        onClick: function(e) {
+            // do the onClick job in TD.
+            // this here is Plugin, this.$el is element where plugin is attached, normally TD.
+            // set it to self so you can use in showInput callback.
+            var self = this,
+                $valueContainer = $("." + self.options.css_value_container_class, self.$el),
+                $inputContainer = $("." + self.options.css_input_container_class, self.$el);
+            if ($inputContainer.is(":hidden")) {
+                // apply showInput and send a callback to bind keydown and focusout events.
+                // in callback this is $inputContainer
+                self._get_func('showInput').apply(self, [$valueContainer, $inputContainer,
+                    function(){
+                        var $input = $inputContainer.find('input');
+                        $input.val(self.value).focus()
+                            .bind('keydown.' + pluginName, $.proxy(self._get_func('onKeyDown'), $input, self))
+                            .bind('focusout.' + pluginName, $.proxy(self._get_func('onFocusOut'), $input, self))
+                            // trigger inputLoad so users can load, e.g., masks to inputs
+                            // remember to use e.target instead this.
+                            .trigger('inputLoad.' + pluginName, [self]);
+                    }]);
+            }
+        },
+
+        onKeyDown: function(self, e) {
+            // handle keydown ESC, TAB or ENTER keypress on input
+            // receives this as input, event and self as plugin.
+            key = e.keyCode ? e.keyCode : e.which;
+            if (key == 13 || key == 9) { // enter or tab, update value
+                e.preventDefault();
+                e.stopPropagation();
+                self._get_func('update').apply(self, [
+                    $(this),
+                    $("." + self.options.css_input_container_class, self.$el),
+                    $("." + self.options.css_value_container_class, self.$el)
+                ]);
+                return false;
+            } else if (key == 27) { // esc, revert value
+                e.preventDefault();
+                e.stopPropagation();
+                self._get_func('dismiss').apply(self, [
+                    $("." + self.options.css_value_container_class, self.$el),
+                    $("." + self.options.css_input_container_class, self.$el)
+                ]);
+                return false;
+            }
+        },
+
+        onFocusOut: function(self, e) {
+            // handle lost of focus by element closing it.
+            // receives this as input, event and self as plugin.
+            self._get_func('dismiss').apply(self, [
+                $("." + self.options.css_value_container_class, self.$el),
+                $("." + self.options.css_input_container_class, self.$el)
+            ]);
+        },
+
+        update: function($input, $inputContainer, $valueContainer) {
+            // called when changes will be updated (ENTER or TAB)
+            // this is the plugin, we receive $input, $inputContainer, $valueContainer
+            if ($inputContainer.is(":visible")) {
+                this.value = $input.val();
+                $valueContainer.text(this.value);
+                this._get_func('hideInput').apply(this, [$valueContainer, $inputContainer,
+                    function() {
+                        $inputContainer.find("input").val('')
+                            .unbind('keydown.' + pluginName)
+                            .unbind('focusout.' + pluginName)
+                            .trigger('inputUnload.' + pluginName);
+                    }]);
+            }
+        },
+
+        dismiss: function($valueContainer, $inputContainer) {
+            // called when changes will be dismissed (ESC or focusout event)
+            // 'this' is the plugin, we receive $valueContainer and $inputContainer
+            if ($inputContainer.is(":visible")) {
+                this._get_func('hideInput').apply(this, [$valueContainer, $inputContainer,
+                    function(){
+                        $inputContainer.find('input')
+                            .val('') // reset input
+                            .unbind('keydown.' + pluginName)
+                            .unbind('focusout.' + pluginName)
+                            // trigger inputLoad so users can unload some stuff
+                            // remember to use e.target instead this.
+                            .trigger('inputUnload.' + pluginName);
+                    }]);
+            }
+        },
+
+        showInput: function($valueContainer, $inputContainer, callback) {
+            /* Hide value container and show input container
+             * Receive two containers as jquery objets
+             * Returns nothing
+             */
+            $valueContainer.fadeOut(function() { $inputContainer.fadeIn(callback); });
+        },
+
+        hideInput: function($valueContainer, $inputContainer, callback) {
+            // hide input container and show value container
+            $inputContainer.fadeOut(function() { $valueContainer.fadeIn(callback); });
         }
     };
 
